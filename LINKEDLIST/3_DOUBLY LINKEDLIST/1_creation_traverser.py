9d4fe23fd61e01b3b0fdc1b121da1fd598244a2f
SHRUTIKA TAYDE <89908129+shrutikatayde@users.noreply.github.com>
2023-10-16 13:43:41 +0530

    doubly linked list

---
+++ LINKEDLIST/3_DOUBLY LINKEDLIST/1_creation_traverser.py
@@ -0,0 +1,41 @@
+class Node:
+    def __init__(self, data, next=None, prev=None):
+        self.data = data
+        self.next = next
+        self.prev = prev
+
+
+class LinkedList:
+    def __init__(self, head=None, tail=None):
+        self.head = head
+        self.tail = tail
+
+    def add(self, data):
+        newNode = Node(data)
+        if self.head is None:
+            self.head = newNode
+            self.tail = newNode
+        else:
+            self.tail.next = newNode
+            newNode.prev = self.tail
+            self.tail = newNode
+
+    def printList(self, first=None):
+        self.first = first
+        first = self.head
+        if self.head is None:
+            print("List is empty")
+
+        while first is not None:
+            print(first.data, "->", end=" ")
+            first = first.next
+        print("None")
+
+
+obj = LinkedList()
+obj.add(8)
+obj.printList()
+obj.add(4)
+obj.printList()
+obj.add(6)
+obj.printList()
---
+++ LINKEDLIST/3_DOUBLY LINKEDLIST/2_insertion.py
@@ -0,0 +1,78 @@
+class Node:
+    def __init__(self, data, next=None):
+        self.data = data
+        self.next = next
+
+
+class LinkedList:
+    def __init__(self, head=None, tail=None):
+        self.head = head
+        self.tail = tail
+
+    def addFirst(self, data):
+        newNode = Node(data)
+        if self.head is None:
+            self.head = newNode
+            self.tail = newNode
+        else:
+            newNode.next = self.head
+            self.head.prev = newNode
+            self.head = newNode
+
+    def addLast(self, data):
+        newNode = Node(data)
+        if self.head is None:
+            self.head = newNode
+            self.tail = newNode
+        else:
+            newNode.prev = self.tail
+            self.tail.next = newNode
+            self.tail = newNode
+
+    def addMid(self, data, p):
+        self.p = p
+        newNode = Node(data)
+        if self.head is None:
+            self.head = newNode
+            self.tail = newNode
+        else:
+            prevNode = self.head
+            currentNode = prevNode.next
+
+            for i in range(1, p - 1):
+                prevNode = currentNode
+                currentNode = currentNode.next
+            newNode.prev = prevNode
+            newNode.next = prevNode.next
+            prevNode.next = newNode
+            currentNode.prev = newNode
+
+    def printList(self, first=None):
+        self.first = first
+        first = self.head
+        if self.head is None:
+            print("list is empty")
+        while first:
+            print(first.data, "->", end=" ")
+            first = first.next
+        print("None")
+
+
+obj = LinkedList()
+obj.addFirst(8)
+obj.printList()
+obj.addFirst(7)
+obj.printList()
+obj.addFirst(6)
+obj.printList()
+
+
+obj.addLast(8)
+obj.printList()
+obj.addLast(7)
+obj.printList()
+obj.addLast(6)
+obj.printList()
+
+obj.addMid(65, 0)
+obj.printList()
